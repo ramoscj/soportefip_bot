ff58d99e42750a9858d275776d6e1d497365b2d1
Carlos Ramos <ramoscj@users.noreply.github.com>
2020-07-18 22:01:09 -0400

    Carlos Ramos
    test con SH

--- test_bot.py
+++ test_bot.py
@@ -3,7 +3,9 @@ import subprocess
 # completed = subprocess.call('echo $PATH', shell=True)
 # print('returncode:', completed)
 
-cp = subprocess.run(["mkdir", "prueba"],shell=True)
-
+# , '-s', '<', 'D:\Python\my_key.dat'
+programa = 'vpncli.exe'
+cp = subprocess.run(['C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\%s' % programa, '-s', '<', 'D:\Python\my_key.dat'], shell=True)
+# print('"C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\%s"' % programa)
 if cp.returncode:
     print(cp.returncode)
