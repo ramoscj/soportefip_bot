bba6c29d1f1a72213abfcb469ab2bdbee3865c06
Carlos Ramos <ramoscj@users.noreply.github.com>
2020-07-22 15:10:25 -0400

    Carlos Ramos
    Actualizando TOKEN

--- bot_token.py
+++ bot_token.py
@@ -4,8 +4,8 @@ def TOKEN():
     sistema = platform.platform()
     if sistema.startswith('Windows-10'):
         # Desarrollo
-        token = 'NzI3OTExMzA2Mjg3MzgyNjU4.Xvy-GA.V1I7jAtOvunlD9I2_J-DZyDRGcg'
+        token = 'NzI3OTExMzA2Mjg3MzgyNjU4.XxiOzg.3Vott2ezR3sotgMrukP1QYOzZYA'
     else:
         # Produccion
-        token = 'NzExOTkwMzYxMTY5NDYxMjk4.Xv4SZQ.sQOuiP9yqLz6xb8FJSQSWFrq9Jk'
+        token = 'NzExOTkwMzYxMTY5NDYxMjk4.Xxhn4w.PxtSwlvIWaxmk-dJMzxrzxs4gIw'
     return token
