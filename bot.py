d87bff1b925de5590b32445f094a8ba51682bdf0
Carlos Ramos <ramoscj@users.noreply.github.com>
2020-08-18 17:44:29 -0400

    Carlos Ramos
    Update

--- bot.py
+++ bot.py
@@ -200,6 +200,7 @@ async def vpnActiveLinux(ctx):
 	try:
 		cp = subprocess.run('sudo bash /usr/bin/start_vpn.sh', shell=True)
 		await ctx.channel('Conectando VPN: %s' % cp.returncode)
+		conexionDB = conexion()
 		await ctx.send('Conexion con DB: %s OK' % str(conexionDB))
 	except Exception as e:
 		await ctx.send('Error VPN: %s' % e)
