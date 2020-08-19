79b21a65b87ae2a4b5a340d6dbe21a8e2496e7de
Carlos Ramos <ramoscj@users.noreply.github.com>
2020-08-18 21:01:14 -0400

    Carlos Ramos
    Path global

--- config_bot.py
+++ config_bot.py
@@ -1,14 +1,21 @@
 import os
+import platform
 
 PATRIMONIOS_TC = {4: 2, 6: 5, 8: 7, 10: 0}
 
 ENTORNO_FIP = {'DB': 'FIP', 'DBLINK': ''}
 ENTORNO_REPORTS = {'DB': 'REPORTS', 'DBLINK': '@kamet.din.cl'}
 
-PAT_BOT = {'PATH': str(os.getcwd())}
-
 CORREOS = {'TO': 'richard.ruiz@adretail.cl', 
         'TO2': 'sop01@imagicair.cl', 
         'CC': ['operaciones@abcdin.cl', 'axel.riobo@imagicair.cl', 'eduardo@imagicair.cl'], 
         'CC2': ['carlos.ramos@imagicair.cl',]
     }
+
+sistema = platform.platform()
+if sistema.startswith('Windows-10'):
+    SO = str(os.getcwd())
+else:
+    SO = '/home/ubuntu/bot/soportefip_bot'
+PAT_BOT = {'PATH': SO}
+print(PAT_BOT['PATH'])
